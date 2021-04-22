135651099a670fbeebd35ae733597272cca8f29f
lvtao <lvtao@ttjianbao.com>
2021-04-22 14:32:48 +0800

    m

--- js/index.js
+++ js/index.js
@@ -1131,6 +1131,10 @@ var vue = new Vue({
                         message: '每周分享04（2021年4月第2期）',
                         url: 'https://mp.weixin.qq.com/s/8UXx-Zgc9ZQAcI22pTOyaw'
                     },
+                    {
+                        message: '每周分享05（2021年4月第3期）',
+                        url: 'https://mp.weixin.qq.com/s/s3vnedr-rCqoXgvBD4csGg'
+                    },
                 
                 ]
             }
